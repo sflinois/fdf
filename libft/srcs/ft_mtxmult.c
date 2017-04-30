fec17e68278ccf79b7230f7075ec40c5c6b5a15b
Sylvain FLINOIS <sflinois@e2r7p5.42.fr>
2017-04-30 16:11:43 +0200

    lib matrices fct

--- libft/srcs/ft_mtxmult.c
+++ libft/srcs/ft_mtxmult.c
@@ -6,7 +6,7 @@
 /*   By: sflinois <sflinois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/29 14:34:11 by sflinois          #+#    #+#             */
-/*   Updated: 2017/04/29 15:52:04 by sflinois         ###   ########.fr       */
+/*   Updated: 2017/04/29 17:32:37 by sflinois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,21 @@ int			calc_mtx(t_matrix *a, t_matrix *b, int x, int y)
 	while (i < a->nb_col)
 	{
 		ret += a->m[x][i] * b->m[i][y];
+		i++;
+	}
+	return (ret);
+}
+
+int			calc_mtx4(int a[4][4], int b[4][4], int x, int y)
+{
+	int		i;
+	int		ret;
+
+	ret = 0;
+	i = 0;
+	while (i < 4)
+	{
+		ret += a[x][i] * b[i][y];
 		i++;
 	}
 	return (ret);
@@ -61,7 +76,27 @@ t_matrix	*ft_mtxmult(t_matrix *a, t_matrix *b)
 	return (NULL);
 }
 
-t_matrix	*ft_mtxmultint(int	a, t_matrix *b)
+int				**ft_mtx4mult(int a[4][4], int b[4][4])
+{
+	int			mtx[4][4];
+	int			x;
+	int			y;
+
+	y = 0;
+	while (y < 4)
+	{
+		x = 0;
+		while (x < 4)
+		{
+			mtx[y][x] = calc_mtx4(a, b, x, y);
+			x++;
+		}
+		y++;
+	}
+	return (mtx);
+}
+
+t_matrix	*ft_mtxmultint(int a, t_matrix *b)
 {
 	t_matrix	*mtx;
 	int			x;
@@ -90,4 +125,24 @@ t_matrix	*ft_mtxmultint(int	a, t_matrix *b)
 		}	
 	}
 	return (NULL);
+}
+
+int				ft_mtx4multint(int a,int b[4][4])
+{
+	int			mtx[4][4];
+	int			x;
+	int			y;
+
+	y = 0;
+	while (y < 4)
+	{
+		x = 0;
+		while (x < 4)
+		{
+			mtx[y][x] = b[y][x] * a;
+			x++;
+		}
+		y++;
+	}
+	return (mtx);
 }
